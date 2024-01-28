5ad6900c4f0b92472951939bcdf6ddc737fb4769
M Abdus Shakur <mabdusshakur12@gmail.com>
2024-01-28 13:02:55 +0600

    auth get routes added

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,10 @@
 <?php
 
+use App\Livewire\Auth\EmailVerificationPage;
+use App\Livewire\Auth\ForgotPasswordPage;
+use App\Livewire\Auth\LoginPage;
+use App\Livewire\Auth\RegistrationPage;
+use App\Livewire\Auth\ResetPasswordPage;
 use App\Livewire\DashboardPage;
 use Illuminate\Support\Facades\Route;
 
@@ -20,3 +25,8 @@ Route::get('/', function () {
 
 
 Route::get('/dashboard', DashboardPage::class)->name('dashboard');
+Route::get('/login', LoginPage::class)->name('login');
+Route::get('/registration', RegistrationPage::class)->name('registration');
+Route::get('/email-verification', EmailVerificationPage::class)->name('email-verification');
+Route::get('/forgot-password', ForgotPasswordPage::class)->name('forgot-password');
+Route::get('/reset-password', ResetPasswordPage::class)->name('reset-password');
