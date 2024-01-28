0746f6e01e12d75e988acb60760024f36835f624
M Abdus Shakur <mabdusshakur12@gmail.com>
2024-01-28 22:43:16 +0600

    registration validation added, both server-client

--- app/Livewire/Auth/RegistrationPage.php
+++ app/Livewire/Auth/RegistrationPage.php
@@ -2,10 +2,30 @@
 
 namespace App\Livewire\Auth;
 
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 use Livewire\Component;
 
 class RegistrationPage extends Component
 {
+    public $name, $email, $password, $password_confirmation;
+
+    protected $rules = [
+        'name' => 'required|min:3|max:50',
+        'email' => 'required|email|unique:users',
+        'password' => 'required|min:8|confirmed',
+    ];
+
+    public function updated($propertyName)
+    {
+        $this->validateOnly($propertyName);
+    }
+    public function registration()
+    {
+        $this->validate();
+        dd($this->name, $this->email, $this->password, $this->password_confirmation);
+    }
+
     public function render()
     {
         return view('livewire.auth.registration-page')->layout('components.layouts.auth');
--- resources/views/livewire/auth/registration-page.blade.php
+++ resources/views/livewire/auth/registration-page.blade.php
@@ -1,18 +1,18 @@
-<body class="bg-register">
+<div class="bg-register">
     <div class="container-fluid my-5">
         <div class="row">
             <div class="col-12 col-md-8 col-lg-6 col-xl-5 col-xxl-4 mx-auto">
-                <div class="card rounded-4">
+                <div class="card rounded-4 mt-4">
                     <div class="card-body p-5">
-                        <img src="{{ asset('assets/images/logo1.png') }}" class="mb-4" width="145" alt="">
                         <h4 class="fw-bold">Get Started Now</h4>
                         <p class="mb-0">Enter your credentials to create your account</p>
 
                         <div class="form-body my-4">
-                            <form class="row g-3">
+                            <form class="row g-3" wire:submit.prevent="registration">
                                 <div class="col-12">
                                     <label for="name" class="form-label">Name</label>
-                                    <input type="text" class="form-control @error('name') is-invalid @enderror" id="name" placeholder="jhon">
+                                    <input type="text" class="form-control @error('name') is-invalid @enderror"
+                                        id="name" placeholder="jhon" wire:model.live.debounce.250ms="name">
                                     @error('name')
                                         <div class="invalid-feedback">
                                             {{ $message }}
@@ -22,7 +22,9 @@
 
                                 <div class="col-12">
                                     <label for="email" class="form-label">Email</label>
-                                    <input type="email" class="form-control @error('email') is-invalid @enderror" id="email" placeholder="jhon@example.com">
+                                    <input type="email" class="form-control @error('email') is-invalid @enderror"
+                                        id="email" placeholder="jhon@example.com"
+                                        wire:model.live.debounce.250ms="email">
                                     @error('email')
                                         <div class="invalid-feedback">
                                             {{ $message }}
@@ -33,22 +35,28 @@
                                 <div class="col-12">
                                     <label for="password" class="form-label">Password</label>
                                     <div class="input-group" id="show_hide_password">
-                                        <input type="password" class="form-control border-end-0 @error('password') is-invalid @enderror" id="password" placeholder="Enter Password">
+                                        <input type="password"
+                                            class="form-control border-end-0 @error('password') is-invalid @enderror"
+                                            id="password" placeholder="Enter Password"
+                                            wire:model.live.debounce.250ms="password">
                                         <a href="javascript:;" class="input-group-text bg-transparent">
                                             <i class="bi bi-eye-slash-fill"></i>
                                         </a>
+                                        @error('password')
+                                            <div class="invalid-feedback">
+                                                {{ $message }}
+                                            </div>
+                                        @enderror
                                     </div>
-                                    @error('password')
-                                        <div class="invalid-feedback">
-                                            {{ $message }}
-                                        </div>
-                                    @enderror
                                 </div>
 
                                 <div class="col-12">
-                                    <label for="confirm_password" class="form-label">Confirm Password</label>
-                                    <input type="password" class="form-control @error('confirm_password') is-invalid @enderror" id="confirm_password" placeholder="Re Enter Password">
-                                    @error('confirm_password')
+                                    <label for="password_confirmation" class="form-label">Confirm Password</label>
+                                    <input type="password"
+                                        class="form-control @error('password_confirmation') is-invalid @enderror"
+                                        id="password_confirmation" placeholder="Re Enter Password"
+                                        wire:model.live.debounce.250ms="password_confirmation">
+                                    @error('password_confirmation')
                                         <div class="invalid-feedback">
                                             {{ $message }}
                                         </div>
@@ -62,24 +70,25 @@
                                 </div>
                                 <div class="col-12">
                                     <div class="text-start">
-                                        <p class="mb-0">Already have an account? <a href="{{ route('login') }}">Login here</a></p>
+                                        <p class="mb-0">Already have an account? <a href="{{ route('login') }}">Login
+                                                here</a></p>
                                     </div>
                                 </div>
                             </form>
                         </div>
 
                         {{-- <div class="separator section-padding">
-                            <div class="line"></div>
-                            <p class="mb-0 fw-bold">OR</p>
-                            <div class="line"></div>
-                        </div>
-
-                        <div class="d-flex gap-3 justify-content-center mt-4">
-                            <a href="javascript:;"
-                                class="wh-48 d-flex align-items-center justify-content-center rounded-circle border">
-                                <img src="{{ asset('assets/images/apps/google_no_bg.png') }}" width="30" alt="">
-                            </a>
-                        </div> --}}
+                                <div class="line"></div>
+                                <p class="mb-0 fw-bold">OR</p>
+                                <div class="line"></div>
+                            </div>
+    
+                            <div class="d-flex gap-3 justify-content-center mt-4">
+                                <a href="javascript:;"
+                                    class="wh-48 d-flex align-items-center justify-content-center rounded-circle border">
+                                    <img src="{{ asset('assets/images/apps/google_no_bg.png') }}" width="30" alt="">
+                                </a>
+                            </div> --}}
                     </div>
                 </div>
             </div>
@@ -105,4 +114,4 @@
             });
         });
     </script>
-</body>
+</div>
