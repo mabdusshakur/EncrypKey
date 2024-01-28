fe9a75ba53aeb2292bb08cee081efd11f8ce865a
M Abdus Shakur <mabdusshakur12@gmail.com>
2024-01-28 12:10:26 +0600

    auth layout added

---
+++ resources/views/components/layouts/auth.blade.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" data-bs-theme="dark">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>{{ $title ?? 'Auth Page' }}</title>
+    <!--favicon-->
+    <link rel="icon" href="{{ asset('assets/images/favicon-32x32.png') }}" type="image/png">
+
+    <!--plugins-->
+    <link href="{{ asset('assets/plugins/perfect-scrollbar/css/perfect-scrollbar.css') }}" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets/plugins/metismenu/metisMenu.min.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets/plugins/metismenu/mm-vertical.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets/plugins/simplebar/css/simplebar.css') }}">
+    <!--bootstrap css-->
+    <link href="{{ asset('assets/css/bootstrap.min.css') }}" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans:wght@300;400;500;600&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Material+Icons+Outlined" rel="stylesheet">
+    <!--main css-->
+    <link href="{{ asset('assets/css/bootstrap-extended.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/sass/main.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/sass/dark-theme.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/sass/responsive.css') }}" rel="stylesheet">
+
+    {{-- Fontawesome --}}
+    <link href="{{ asset('assets/plugins/fontawesome-free-6.4.2-web/css/all.min.css') }}" rel="stylesheet" />
+
+</head>
+
+{{ $slot }}
+
+</html>
