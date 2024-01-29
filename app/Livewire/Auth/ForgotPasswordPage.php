9f31f043bc6b51f193b120f48ec42468e5aa61a0
M Abdus Shakur <mabdusshakur12@gmail.com>
2024-01-29 23:28:24 +0600

    forgot password customized

--- app/Livewire/Auth/ForgotPasswordPage.php
+++ app/Livewire/Auth/ForgotPasswordPage.php
@@ -6,6 +6,21 @@ use Livewire\Component;
 
 class ForgotPasswordPage extends Component
 {
+    public $email;
+
+    protected $rules = [
+        'email' => 'required|email',
+    ];
+
+    public function updated($propertyName)
+    {
+        $this->validateOnly($propertyName);
+    }
+
+    public function forgotPassword()
+    {
+        session()->flash('success', 'Password reset link sent on your email.');
+    }
     public function render()
     {
         return view('livewire.auth.forgot-password-page')->layout('components.layouts.auth');
--- resources/views/livewire/auth/forgot-password-page.blade.php
+++ resources/views/livewire/auth/forgot-password-page.blade.php
@@ -2,16 +2,19 @@
     <div class="container-fluid my-5">
         <div class="row">
             <div class="col-12 col-md-8 col-lg-6 col-xl-5 col-xxl-4 mx-auto">
+                @include('components.helper.alert')
                 <div class="card rounded-4">
                     <div class="card-body p-5">
                         <h4 class="fw-bold">Forgot Password</h4>
                         <p class="mb-0">Enter your registered email to reset your password</p>
-                        
+
                         <div class="form-body my-4">
-                            <form class="row g-3">
+                            <form class="row g-3" wire:submit.prevent="forgotPassword">
                                 <div class="col-12">
                                     <label for="email" class="form-label">Email</label>
-                                    <input type="email" class="form-control @error('email') is-invalid @enderror" id="email" placeholder="jhon@example.com">
+                                    <input type="email" class="form-control @error('email') is-invalid @enderror"
+                                        id="email" placeholder="jhon@example.com"
+                                        wire:model.live.debounce.250ms="email">
                                     @error('email')
                                         <div class="invalid-feedback">
                                             {{ $message }}
