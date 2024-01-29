c76474f00476f75a79f9d76ac53e7c3f139716f5
M Abdus Shakur <mabdusshakur12@gmail.com>
2024-01-29 23:27:57 +0600

    elerts added

---
+++ resources/views/components/helper/alert.blade.php
@@ -0,0 +1,17 @@
+@if (Session::has('success'))
+    <div class="d-flex flex-column gap-2" id="helper-alert">
+        <div class="alert alert-success border-0 bg-success alert-dismissible fade show">
+            <div class="text-white">{{ Session::get('success') }}</div>
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        </div>
+    </div>
+@endif
+
+@if (Session::has('error'))
+    <div class="d-flex flex-column gap-2" id="helper-alert">
+        <div class="alert alert-danger border-0 bg-danger alert-dismissible fade show">
+            <div class="text-white">{{ Session::get('success') }}</div>
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        </div>
+    </div>
+@endif
