0e25de065a3f5668f2e96c485bf329647a35261a
M Abdus Shakur <mabdusshakur12@gmail.com>
2024-01-30 13:39:10 +0600

    reset password completed

--- app/Livewire/Auth/ResetPasswordPage.php
+++ app/Livewire/Auth/ResetPasswordPage.php
@@ -2,10 +2,53 @@
 
 namespace App\Livewire\Auth;
 
+use App\Models\ResetPassword;
+use App\Models\User;
 use Livewire\Component;
 
 class ResetPasswordPage extends Component
 {
+    public $token, $password, $password_confirmation;
+
+    protected $rules = [
+        'password' => 'required|min:8|confirmed',
+    ];
+
+    public function mount()
+    {
+        $this->token = request()->query('token');
+        $isTokenExists = ResetPassword::where('token', $this->token)->exists();
+        $isTokenUsed = ResetPassword::where('token', $this->token)->where('is_used', true)->exists();
+        if (!$isTokenExists || $isTokenUsed) {
+            session()->flash('error', 'Invalid token!');
+            return redirect()->route('forgot-password');
+        }
+    }
+
+    public function updated($propertyName)
+    {
+        $this->validateOnly($propertyName);
+    }
+
+    public function resetPassword()
+    {
+        $this->validate();
+        $ResetPassword = ResetPassword::where('token', $this->token)->update([
+            'is_used' => true,
+            'used_at' => now(),
+        ]);
+        if (!$ResetPassword) {
+            session()->flash('error', 'Something went wrong!');
+            return redirect()->route('forgot-password');
+        }
+        $requestedUsed = ResetPassword::where('token', $this->token)->first();
+        $user = User::where('email', $requestedUsed->email)->first();
+        $user->update([
+            'password' => bcrypt($this->password),
+        ]);
+        session()->flash('success', 'Password reset successfully!');
+        return redirect()->route('login');
+    }
     public function render()
     {
         return view('livewire.auth.reset-password-page')->layout('components.layouts.auth');
--- resources/views/livewire/auth/reset-password-page.blade.php
+++ resources/views/livewire/auth/reset-password-page.blade.php
@@ -2,25 +2,43 @@
     <div class="container-fluid my-5">
         <div class="row">
             <div class="col-12 col-md-8 col-lg-6 col-xl-5 col-xxl-4 mx-auto">
+                @include('components.helper.alert')
                 <div class="card rounded-4">
                     <div class="card-body p-5">
                         <h4 class="fw-bold">Reset Password</h4>
                         <p class="mb-0">Enter your new password</p>
-                        
+
                         <div class="form-body my-4">
-                            <form class="row g-3">
+                            <form class="row g-3" wire:submit.prevent="resetPassword">
                                 <div class="col-12">
-                                    <label for="password" class="form-label">New Password</label>
-                                    <input type="password" class="form-control @error('password') is-invalid @enderror" id="password" placeholder="Enter your new password">
-                                    @error('password')
+                                    <label for="password" class="form-label">Password</label>
+                                    <div class="input-group" id="show_hide_password">
+                                        <input type="password"
+                                            class="form-control border-end-0 @error('password') is-invalid @enderror"
+                                            id="password" placeholder="Enter Password"
+                                            wire:model.live.debounce.250ms="password">
+                                        <a href="javascript:;" class="input-group-text bg-transparent">
+                                            <i class="bi bi-eye-slash-fill"></i>
+                                        </a>
+                                        @error('password')
+                                            <div class="invalid-feedback">
+                                                {{ $message }}
+                                            </div>
+                                        @enderror
+                                    </div>
+                                </div>
+
+                                <div class="col-12">
+                                    <label for="password_confirmation" class="form-label">Confirm Password</label>
+                                    <input type="password"
+                                        class="form-control @error('password_confirmation') is-invalid @enderror"
+                                        id="password_confirmation" placeholder="Re Enter Password"
+                                        wire:model.live.debounce.250ms="password_confirmation">
+                                    @error('password_confirmation')
                                         <div class="invalid-feedback">
                                             {{ $message }}
                                         </div>
                                     @enderror
-                                </div>
-                                <div class="col-12">
-                                    <label for="confirm_password" class="form-label">Confirm Password</label>
-                                    <input type="password" class="form-control" id="confirm_password" placeholder="Confirm your new password">
                                 </div>
                                 <div class="col-12">
                                     <div class="d-grid">
