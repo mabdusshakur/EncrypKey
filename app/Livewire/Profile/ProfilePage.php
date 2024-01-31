fe493f0e350197d3e0d6e5578d2b575081502c47
M Abdus Shakur <mabdusshakur12@gmail.com>
2024-01-31 22:49:30 +0600

    profile-page component added

---
+++ app/Livewire/Profile/ProfilePage.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Profile;
+
+use Livewire\Component;
+
+class ProfilePage extends Component
+{
+    public function render()
+    {
+        return view('livewire.profile.profile-page');
+    }
+}
