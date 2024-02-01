4c9571c7a8289a6908a48ffddd5003d3bea18e4f
M Abdus Shakur <mabdusshakur12@gmail.com>
2024-02-01 13:48:18 +0600

    applications page added

---
+++ app/Livewire/Application/ApplicationPage.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Application;
+
+use Livewire\Component;
+
+class ApplicationPage extends Component
+{
+    public function render()
+    {
+        return view('livewire.application.application-page');
+    }
+}
