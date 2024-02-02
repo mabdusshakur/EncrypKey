eed77f4428ad604a74ed5cc62be5f22b5d4a2fd9
M Abdus Shakur <mabdusshakur12@gmail.com>
2024-02-02 12:16:31 +0600

    create app backends defined

--- app/Livewire/Application/ApplicationCreate.php
+++ app/Livewire/Application/ApplicationCreate.php
@@ -6,6 +6,23 @@ use Livewire\Component;
 
 class ApplicationCreate extends Component
 {
+    public $name;
+
+    protected $rules = [
+        'name' => 'required|min:3',
+    ];
+
+    public function updated($propertyName)
+    {
+        $this->validateOnly($propertyName);
+    }
+
+    public function createApp()
+    {
+        $this->validate();
+        // Create application
+    }
+    
     public function render()
     {
         return view('livewire.application.application-create');
