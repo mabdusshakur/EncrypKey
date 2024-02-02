0c70ad9e9450dc57f50fbd7ed548197847212d39
M Abdus Shakur <mabdusshakur12@gmail.com>
2024-02-02 11:55:29 +0600

    application card componente added

---
+++ app/Livewire/Application/ApplicationCard.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Application;
+
+use Livewire\Component;
+
+class ApplicationCard extends Component
+{
+    public function render()
+    {
+        return view('livewire.application.application-card');
+    }
+}
