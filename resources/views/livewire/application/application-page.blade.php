316a4fed4ce0b6ae8fde0e01b58cd28c6860590f
M Abdus Shakur <mabdusshakur12@gmail.com>
2024-02-02 12:14:54 +0600

    create app form imported

--- resources/views/livewire/application/application-page.blade.php
+++ resources/views/livewire/application/application-page.blade.php
@@ -1,4 +1,10 @@
 <div>
+    <div class="row mb-2 d-felx justify-content-center">
+        <div class="col-12 col-xl-4 col-md-6">
+            @livewire('application.application-create')
+        </div>
+    </div>
+    <hr/>
     <div class="row g-4">
         @livewire('application.application-card')
     </div>
