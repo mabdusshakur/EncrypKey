d5eb2450d4105a3a0f52fcf0fb0558f4bfd01ca7
M Abdus Shakur <mabdusshakur12@gmail.com>
2024-02-02 12:14:46 +0600

    create application form added

--- resources/views/livewire/application/application-create.blade.php
+++ resources/views/livewire/application/application-create.blade.php
@@ -1,3 +1,20 @@
 <div>
-    {{-- The Master doesn't talk, he acts. --}}
+    <form class="row g-3" wire:submit.prevent="createApp">
+        <div class="col-12">
+            <label for="app" class="form-label">Application Name</label>
+            <input type="text" class="form-control @error('name') is-invalid @enderror" id="name" placeholder="My App"
+                wire:model.live.debounce.250ms="name">
+            @error('name')
+                <div class="invalid-feedback">
+                    {{ $message }}
+                </div>
+            @enderror
+        </div>
+
+        <div class="col-12">
+            <div class="d-grid">
+                <button type="submit" class="btn btn-outline-primary">Create App</button>
+            </div>
+        </div>
+    </form>
 </div>
