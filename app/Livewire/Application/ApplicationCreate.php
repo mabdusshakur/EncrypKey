ff70649a6e934a7f36cb05e1b872310611190906
M Abdus Shakur <mabdusshakur12@gmail.com>
2024-02-02 11:55:38 +0600

    application create componente added

---
+++ app/Livewire/Application/ApplicationCreate.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Application;
+
+use Livewire\Component;
+
+class ApplicationCreate extends Component
+{
+    public function render()
+    {
+        return view('livewire.application.application-create');
+    }
+}
