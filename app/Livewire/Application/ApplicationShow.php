90f6383f3d79c67b2feaa92d6e104d20bd47e439
M Abdus Shakur <mabdusshakur12@gmail.com>
2024-02-02 11:55:44 +0600

    application show componente added

---
+++ app/Livewire/Application/ApplicationShow.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Application;
+
+use Livewire\Component;
+
+class ApplicationShow extends Component
+{
+    public function render()
+    {
+        return view('livewire.application.application-show');
+    }
+}
