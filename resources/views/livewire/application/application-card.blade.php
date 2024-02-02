653e3bf81d811876fcbe0e91caf8ae5b3fb98c6f
M Abdus Shakur <mabdusshakur12@gmail.com>
2024-02-02 12:07:53 +0600

    card added

--- resources/views/livewire/application/application-card.blade.php
+++ resources/views/livewire/application/application-card.blade.php
@@ -1,3 +1,27 @@
 <div>
-    {{-- Because she competes with no one, no one can compete with her. --}}
+    <div class="col-12 col-xl-4 col-md-6">
+        <div class="card">
+            <div class="card-body">
+                <div class="text-center">
+                    <img src="{{ asset('assets/images/logo.png') }}" width="60" height="60"
+                        class="rounded-circle raised bg-white" alt="">
+                </div>
+                <div class="text-center mt-4">
+                    <h5 class="mb-2">My App License</h5>
+                </div>
+                <div class="d-flex align-items-center justify-content-around mt-5">
+                    <div class="d-flex flex-column gap-2">
+                        <h6 class="mb-0">License : 798</h6>
+                    </div>
+                    <div class="d-flex flex-column gap-2">
+                        <h6 class="mb-0">Used : 546</h6>
+                    </div>
+                </div>
+                <hr>
+                <div class="d-flex align-items-center justify-content-center gap-3">
+                    <button class="btn btn-primary">Manage</button>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
