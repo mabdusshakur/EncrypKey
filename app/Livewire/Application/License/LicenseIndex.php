f74f9551f5b60edb8997b4d91b505ad3f51eb2d0
M Abdus Shakur <mabdusshakur12@gmail.com>
2024-02-03 15:15:59 -0800

    dynamic licenses retrieved on table

--- app/Livewire/Application/License/LicenseIndex.php
+++ app/Livewire/Application/License/LicenseIndex.php
@@ -2,11 +2,20 @@
 
 namespace App\Livewire\Application\License;
 
+use App\Models\License;
+use Livewire\Attributes\On;
 use Livewire\Component;
 
 class LicenseIndex extends Component
 {
-    public $license;
+    public $licenses;
+    public $application;
+
+    #[On('license-created')]
+    public function mount()
+    {
+        $this->licenses = License::where('application_id', $this->application->id)->get();
+    }
     public function render()
     {
         return view('livewire.application.license.license-index');
--- resources/views/livewire/application/license/license-index.blade.php
+++ resources/views/livewire/application/license/license-index.blade.php
@@ -15,19 +15,21 @@
                         </tr>
                     </thead>
                     <tbody>
-                        <tr>
-                            <td>KJHOI-XKJXOS-IOLOE-SFSEWE</td>
-                            <td>Used</td>
-                            <td>Oh98wf-ewfewrjg-ergreger-dfgdg</td>
-                            <td>192.168.0.1</td>
-                            <td>255-255-255-255</td>
-                            <td>2024/02/10</td>
-                            <td>
-                                <button class="btn btn-sm btn-danger">Delete</button>
-                                <button class="btn btn-sm btn-warning">Ban</button>
-                                <button class="btn btn-sm btn-success">Unban</button>
-                            </td>
-                        </tr>
+                        @foreach ($licenses as $license)
+                            <tr>
+                                <td>{{$license->license_key}}</td>
+                                <td>{{$license->is_used ? 'Used' : 'Un-Used'}}</td>
+                                <td>{{$license->hwid_hash}}</td>
+                                <td>{{$license->ip_address}}</td>
+                                <td>{{$license->mac_address}}</td>
+                                <td>{{$license->expires_at}}</td>
+                                <td>
+                                    <button class="btn btn-sm btn-danger">Delete</button>
+                                    <button class="btn btn-sm btn-warning">Ban</button>
+                                    <button class="btn btn-sm btn-success">Unban</button>
+                                </td>
+                            </tr>
+                        @endforeach
                     </tbody>
                 </table>
             </div>
