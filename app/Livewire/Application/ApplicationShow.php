eee709d7fe6f3f3b8e9cd678cd3fe0cd5ce0626f
M Abdus Shakur <mabdusshakur12@gmail.com>
2024-02-03 14:38:54 -0800

    restricted from different users

--- app/Livewire/Application/ApplicationShow.php
+++ app/Livewire/Application/ApplicationShow.php
@@ -2,10 +2,20 @@
 
 namespace App\Livewire\Application;
 
+use App\Models\Application;
 use Livewire\Component;
 
 class ApplicationShow extends Component
 {
+    public $application;
+    public function mount($application)
+    {
+        $this->application = Application::find($application);
+        $isUserAuthorized = auth()->user()->id === $this->application->user_id;
+        if (!$isUserAuthorized) {
+            return redirect()->route('applications');
+        }
+    }
     public function render()
     {
         return view('livewire.application.application-show');
