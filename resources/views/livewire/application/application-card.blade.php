eb78ef08f6aa8dff4cfbf2084d0d033063df1071
M Abdus Shakur <mabdusshakur12@gmail.com>
2024-02-03 14:22:42 -0800

    card css fixed

--- resources/views/livewire/application/application-card.blade.php
+++ resources/views/livewire/application/application-card.blade.php
@@ -1,23 +1,21 @@
 <div>
-    <div class="col-12 col-xl-4 col-md-6">
-        <div class="card">
-            <div class="card-body">
-                <div class="text-center">
-                    <img src="{{ asset('assets/images/logo.png') }}" width="60" height="60"
-                        class="rounded-circle raised bg-white" alt="">
+    <div class="card">
+        <div class="card-body">
+            <div class="text-center">
+                <img src="{{ asset('assets/images/logo.png') }}" width="60" height="60"
+                    class="rounded-circle raised bg-white" alt="">
+            </div>
+            <div class="text-center mt-4">
+                <h5 class="mb-2">{{ $application->name }}</h5>
+            </div>
+            <div class="d-flex align-items-center justify-content-around mt-5">
+                <div class="d-flex flex-column gap-2">
+                    <h6 class="mb-0">License : {{ $application->license_count }}</h6>
                 </div>
-                <div class="text-center mt-4">
-                    <h5 class="mb-2">My App License</h5>
-                </div>
-                <div class="d-flex align-items-center justify-content-around mt-5">
-                    <div class="d-flex flex-column gap-2">
-                        <h6 class="mb-0">License : 798</h6>
-                    </div>
-                </div>
-                <hr>
-                <div class="d-flex align-items-center justify-content-center gap-3">
-                    <button class="btn btn-primary">Manage</button>
-                </div>
+            </div>
+            <hr>
+            <div class="d-flex align-items-center justify-content-center gap-3">
+                <button class="btn btn-primary">Manage</button>
             </div>
         </div>
     </div>
