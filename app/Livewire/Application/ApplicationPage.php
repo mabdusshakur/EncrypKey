aa84567c6b4cdf644a6195ad85b252284de410eb
M Abdus Shakur <mabdusshakur12@gmail.com>
2024-02-03 14:23:05 -0800

    applications rendered

--- app/Livewire/Application/ApplicationPage.php
+++ app/Livewire/Application/ApplicationPage.php
@@ -2,10 +2,17 @@
 
 namespace App\Livewire\Application;
 
+use App\Models\Application;
 use Livewire\Component;
 
 class ApplicationPage extends Component
 {
+    public $applications;
+
+    public function mount()
+    {
+        $this->applications = Application::where('user_id', auth()->user()->id)->get();
+    }
     public function render()
     {
         return view('livewire.application.application-page');
