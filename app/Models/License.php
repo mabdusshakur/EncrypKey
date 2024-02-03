90f0010b4a4623911f8845f4369f0d4feebb90a8
M Abdus Shakur <mabdusshakur12@gmail.com>
2024-02-02 18:10:55 -0800

    license model added

---
+++ app/Models/License.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class License extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'license_key',
+        'is_banned',
+        'ban_reason',
+        'application_id',
+        'hwid_hash',
+        'ip_address',
+        'mac_address',
+        'country',
+        'isp',
+        'expires_at',
+        'banned_at',
+        'last_login_at'
+    ];
+}
