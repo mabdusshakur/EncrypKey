0e97f387ddb15e156d52b1cbb23b838f00b7cf20
M Abdus Shakur <mabdusshakur12@gmail.com>
2024-02-03 14:48:12 -0800

    license index component added with tables

---
+++ resources/views/livewire/application/license/license-index.blade.php
@@ -0,0 +1,36 @@
+<div>
+    <div class="card">
+        <div class="card-body">
+            <div class="table-responsive">
+                <table id="example" class="table table-striped table-bordered" style="width:100%">
+                    <thead>
+                        <tr>
+                            <th>License</th>
+                            <th>Status</th>
+                            <th>HWID</th>
+                            <th>IP</th>
+                            <th>MAC</th>
+                            <th>Expires At</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>KJHOI-XKJXOS-IOLOE-SFSEWE</td>
+                            <td>Used</td>
+                            <td>Oh98wf-ewfewrjg-ergreger-dfgdg</td>
+                            <td>192.168.0.1</td>
+                            <td>255-255-255-255</td>
+                            <td>2024/02/10</td>
+                            <td>
+                                <button class="btn btn-sm btn-danger">Delete</button>
+                                <button class="btn btn-sm btn-warning">Ban</button>
+                                <button class="btn btn-sm btn-success">Unban</button>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</div>
