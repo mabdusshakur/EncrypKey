706a824d5dc7f50e44cd46b0e77b5985a520bba1
M Abdus Shakur <mabdusshakur12@gmail.com>
2024-02-03 14:22:49 -0800

    application defined

--- app/Livewire/Application/ApplicationCard.php
+++ app/Livewire/Application/ApplicationCard.php
@@ -6,6 +6,7 @@ use Livewire\Component;
 
 class ApplicationCard extends Component
 {
+    public $application;
     public function render()
     {
         return view('livewire.application.application-card');
