957d6bd426a1689f13f64e4742f2740d39f3eebb
M Abdus Shakur <mabdusshakur12@gmail.com>
2024-02-03 17:08:51 +0600

    create application added

--- app/Livewire/Application/ApplicationCreate.php
+++ app/Livewire/Application/ApplicationCreate.php
@@ -2,6 +2,7 @@
 
 namespace App\Livewire\Application;
 
+use App\Models\Application;
 use Livewire\Component;
 
 class ApplicationCreate extends Component
@@ -19,10 +20,23 @@ class ApplicationCreate extends Component
 
     public function createApp()
     {
-        $this->validate();
-        // Create application
+        try {
+            $this->validate();
+            $created = Application::create([
+                'name' => $this->name,
+                'secret' => sha1(time()),
+                'user_id' => auth()->id(),
+            ]);
+            if (!$created) {
+                session()->flash('error', 'Application could not be created');
+            }
+            session()->flash('success', 'Application created successfully');
+            $this->reset('name');
+        } catch (\Throwable $th) {
+            session()->flash('error', $th->getMessage());
+        }
     }
-    
+
     public function render()
     {
         return view('livewire.application.application-create');
