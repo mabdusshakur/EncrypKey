fafefed99f3328d07ef29e398bcc0c1c387a8589
M Abdus Shakur <mabdusshakur12@gmail.com>
2024-02-03 14:29:52 -0800

    event listened

--- app/Livewire/Application/ApplicationPage.php
+++ app/Livewire/Application/ApplicationPage.php
@@ -3,12 +3,14 @@
 namespace App\Livewire\Application;
 
 use App\Models\Application;
+use Livewire\Attributes\On;
 use Livewire\Component;
 
 class ApplicationPage extends Component
 {
     public $applications;
 
+    #[On('application-created')] 
     public function mount()
     {
         $this->applications = Application::where('user_id', auth()->user()->id)->get();
