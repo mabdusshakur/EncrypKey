d53d8033ac641a14dc4ba45e0940831a58b3d0d7
M Abdus Shakur <mabdusshakur12@gmail.com>
2024-02-05 21:15:12 +0600

    application details compo imported

--- resources/views/livewire/application/application-show.blade.php
+++ resources/views/livewire/application/application-show.blade.php
@@ -1,6 +1,12 @@
 <div>
     <div class="row mb-2 d-felx justify-content-center">
         <div class="col-12 col-xl-4 col-md-6">
+            @livewire('application.application-details', ['application' => $application], key($application->id))
+        </div>
+    </div>
+    <hr />
+    <div class="row mb-2 d-felx justify-content-center">
+        <div class="col-12 col-xl-4 col-md-6">
             @livewire('application.license.license-create', ['application' => $application], key($application->id))
         </div>
     </div>
