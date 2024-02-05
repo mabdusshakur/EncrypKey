9306d4413b3381839c31baf4fd9a4d07ef71047a
M Abdus Shakur <mabdusshakur12@gmail.com>
2024-02-05 11:42:44 +0600

    ban,unban,delete working added

--- app/Livewire/Application/License/LicenseIndex.php
+++ app/Livewire/Application/License/LicenseIndex.php
@@ -11,7 +11,7 @@ class LicenseIndex extends Component
     public $licenses;
     public $application;
 
-    #[On('license-created')]
+    #[On(['license-created', 'license-deleted', 'license-banned', 'license-unbanned'])]
     public function mount()
     {
         $this->licenses = License::where('application_id', $this->application->id)->get();
@@ -19,18 +19,42 @@ class LicenseIndex extends Component
 
     public function unbanLicense($id)
     {
-        
+        $license = License::find($id);
+        if ($license) {
+            $license->update([
+                'is_banned' => false,
+                'ban_reason' => null,
+                'banned_at' => null
+            ]);
+            $this->dispatch('license-unbanned');
+            session()->flash('success', 'License unbanned successfully');
+        }
     }
 
     public function banLicense($id)
     {
-        
+        $license = License::find($id);
+        if ($license) {
+            $license->update([
+                'is_banned' => true,
+                'ban_reason' => 'Banned by Admin',
+                'banned_at' => now()
+            ]);
+            $this->dispatch('license-banned');
+            session()->flash('success', 'License banned successfully');
+        }
     }
 
     public function deleteLicense($id)
     {
-        
+        $license = License::find($id);
+        if ($license) {
+            $license->delete();
+            $this->dispatch('license-deleted');
+            session()->flash('success', 'License deleted successfully');
+        }
     }
+    
     public function render()
     {
         return view('livewire.application.license.license-index');
