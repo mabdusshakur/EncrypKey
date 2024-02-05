cc100c0fae08498dd2266ca2852cfaf03475a468
M Abdus Shakur <mabdusshakur12@gmail.com>
2024-02-05 11:31:20 +0600

    default license ban made to false

--- database/migrations/2024_02_03_014754_create_licenses_table.php
+++ database/migrations/2024_02_03_014754_create_licenses_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('license_key');
             $table->boolean('is_used')->default(false);
-            $table->boolean('is_banned')->default(true);
+            $table->boolean('is_banned')->default(false);
             $table->string('ban_reason')->nullable();
             $table->foreignId('application_id')->references('id')->on('applications')->cascadeOnDelete();
             $table->string('hwid_hash')->nullable();
