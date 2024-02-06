1bf001530b27f9614e8ed5b4dc57f872147ba3f3
M Abdus Shakur <mabdusshakur12@gmail.com>
2024-02-06 16:01:24 +0600

    apps, license count card added

--- resources/views/livewire/dashboard-page.blade.php
+++ resources/views/livewire/dashboard-page.blade.php
@@ -1,3 +1,29 @@
 <div>
-   dashboard-page works!
+    <div class="d-flex justify-content-center">
+        <div class="col-12 col-xl-8 d-flex">
+            <div class="card rounded-4 w-100">
+                <div class="card-body">
+                    <div class="d-flex align-items-center justify-content-around flex-wrap gap-4 p-4">
+                        <div class="d-flex flex-column align-items-center justify-content-center gap-2">
+                            <a href="javascript:;"
+                                class="mb-2 wh-48 bg-primary bg-opacity-10 text-primary rounded-circle d-flex align-items-center justify-content-center">
+                                <i class="fa-solid fa-boxes-stacked"></i>
+                            </a>
+                            <h3 class="mb-0">{{ $application_count }}</h3>
+                            <p class="mb-0">Total Applications</p>
+                        </div>
+                        <div class="vr"></div>
+                        <div class="d-flex flex-column align-items-center justify-content-center gap-2">
+                            <a href="javascript:;"
+                                class="mb-2 wh-48 bg-success bg-opacity-10 text-success rounded-circle d-flex align-items-center justify-content-center">
+                                <i class="fa-solid fa-key"></i>
+                            </a>
+                            <h3 class="mb-0">{{ $license_count }}</h3>
+                            <p class="mb-0">Total License</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
