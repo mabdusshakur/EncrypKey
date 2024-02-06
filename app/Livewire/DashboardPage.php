6c2ad13d2b9b117fbe047244819527d02265e5d3
M Abdus Shakur <mabdusshakur12@gmail.com>
2024-02-06 16:01:32 +0600

    current user - app and - all license count added

--- app/Livewire/DashboardPage.php
+++ app/Livewire/DashboardPage.php
@@ -2,10 +2,20 @@
 
 namespace App\Livewire;
 
+use App\Models\Application;
+use App\Models\License;
 use Livewire\Component;
 
 class DashboardPage extends Component
 {
+    public $application_count, $license_count;
+
+    public function mount()
+    {
+        $this->application_count = Application::where('user_id', auth()->id())->count();
+        $applicationIds = auth()->user()->applications->pluck('id');
+        $this->license_count = License::whereIn('application_id', $applicationIds)->count();
+    }
     public function render()
     {
         return view('livewire.dashboard-page');
