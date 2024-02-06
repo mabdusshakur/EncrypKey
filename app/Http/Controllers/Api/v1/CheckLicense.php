d7cfb02911e2fd3b05956a174c1e658e97911b03
M Abdus Shakur <mabdusshakur12@gmail.com>
2024-02-06 11:11:49 +0600

    owner , application , is application owned by the owner, then license available on that application or not, checked.

--- app/Http/Controllers/Api/v1/CheckLicense.php
+++ app/Http/Controllers/Api/v1/CheckLicense.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers\Api\v1;
 
+use App\Models\Application;
+use App\Models\User;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\CheckLicenseRequest;
@@ -11,10 +13,39 @@ class CheckLicense extends Controller
     public function checkLicense(CheckLicenseRequest $request)
     {
         $request->validated();
+
+        // Check if the owner exists
+        $owner = User::where('owner_id', $request->owner_id)->first();
+        if (!$owner) {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Owner not found'
+            ], 404);
+        }
+
+        // Check if the application exists
+        $application = Application::where('name', $request->name)->where('secret', $request->secret)->first();
+        if (!$application) {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Application not found'
+            ], 404);
+        }
+        
+        // Check if the owner has the application
+        $ownerHasApplication = $owner->applications->contains($application->id);
+        if (!$ownerHasApplication) {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Owner does not have this application'
+            ], 404);
+        }
+
+        // Check if the license exists
+        $license = $application->licenses->where('license_key', $request->license_key)->first();
+
         return response()->json([
-            'owner_id' => $request->owner_id,
-            'secret' => $request->secret,
-            'name' => $request->name,
+            $license
         ], 200);
     }
 }
