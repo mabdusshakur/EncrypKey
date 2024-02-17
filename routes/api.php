370f77ef8a2aea585de44f3d8dea5f99de192277
M Abdus Shakur <mabdusshakur12@gmail.com>
2024-02-17 17:18:52 +0600

    cleanup

--- routes/api.php
+++ routes/api.php
@@ -4,15 +4,4 @@ use App\Http\Controllers\Api\v1\CheckLicense;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
-
 Route::post('/check-license', [CheckLicense::class, 'checkLicense']);
