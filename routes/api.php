2c382020aad4949d9433ab32231dbfda5ab940ee
M Abdus Shakur <mabdusshakur12@gmail.com>
2024-02-19 14:41:58 +0600

    Ban License route added

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\Api\V1\BanLicense;
 use App\Http\Controllers\Api\v1\CheckLicense;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/check-license', [CheckLicense::class, 'checkLicense']);
+Route::post('/ban-license', [BanLicense::class, 'banLicense']);
