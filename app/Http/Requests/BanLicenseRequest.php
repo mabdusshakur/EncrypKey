105d251a231760c6a78d31cf8c07be65509eef76
M Abdus Shakur <mabdusshakur12@gmail.com>
2024-02-19 14:44:48 +0600

    ban request added

---
+++ app/Http/Requests/BanLicenseRequest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class BanLicenseRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'owner_id' => 'required|exists:users,owner_id',
+            'secret' => 'required|exists:applications,secret',
+            'name' => 'required|exists:applications,name',
+            'license_key' => 'required|exists:licenses,license_key',
+        ];
+    }
+
+    public function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json([
+            'status' => 'error',
+            'message' => $validator->errors(),
+        ], 401));
+    }
+}
